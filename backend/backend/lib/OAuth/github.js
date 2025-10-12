334380bcf152e22672a07cef70de361a17442613
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-12 23:23:50 +0530

    Reorganized folder structure to match local VS Code layout

--- backend/backend/lib/OAuth/github.js
+++
@@ -1,9 +0,0 @@
-import { GitHub } from "arctic";
-import dotenv from "dotenv"
-dotenv.config();
-
-export const github = new GitHub(
-    process.env.GITHUB_CLIENT_ID,
-    process.env.GITHUB_CLIENT_SECRET,
-    `${process.env.BACKEND_URL}/github/callback`,
-)
--- routes/cartRoute.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express" 
-import * as cartControllers from "../controllers/cartControllers.js";
-
-export const cartRouter = Router();
-
-cartRouter.route("/add").post(cartControllers.addToCart)
-cartRouter.route("/decrease").post(cartControllers.DecreaseFromCart)
-cartRouter.route("/remove").post(cartControllers.removeFromCart)
-cartRouter.route("/get").post(cartControllers.getCart)

17016330b58dcd176fb42a16ca72c3510ccb4dc6
Arshit Gajera <arshitgajera24@gmail.com>
2025-11-06 16:46:56 +0530

    Update Home.jsx

--- admin/src/pages/Home/Home.jsx
+++ admin/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { assets } from '../../assets/assets'
 import "./Home.css"
 
 const Home = () => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <div className='home-admin'>
-        <img src={assets.admin_icon} alt="" />
+        <img src={assets.admin_icon} alt="" fetchpriority="high" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "all 0.3s ease-out"}}  />
     </div>
   )
 }

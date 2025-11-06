053f2ed5ddc6d9d40f0025848100d6fbcddb0626
Arshit Gajera <arshitgajera24@gmail.com>
2025-11-06 16:34:05 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -8,8 +8,8 @@ const AppDownload = () => {
     <div className='app-download' id='app-download'>
         <p>For Better Experince Download <br /> <b>SHARP MART</b> App</p>
         <div className="app-download-platforms">
-            <img onClick={() => window.open("https://play.google.com/", "_blank")} src={assets.play_store} alt="PlayStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
-            <img onClick={() => window.open("https://www.apple.com/in/app-store/", "_blank")} src={assets.app_store} alt="AppStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+            <img onClick={() => window.open("https://play.google.com/", "_blank")} src={assets.play_store} alt="PlayStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "all 0.3s ease-out"}} />
+            <img onClick={() => window.open("https://www.apple.com/in/app-store/", "_blank")} src={assets.app_store} alt="AppStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "all 0.3s ease-out"}} />
         </div>
     </div>
   )

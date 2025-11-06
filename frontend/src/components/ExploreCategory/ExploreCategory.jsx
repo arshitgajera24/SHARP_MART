b1f55cd2131ebc597623b6f3a462c67a33b67386
Arshit Gajera <arshitgajera24@gmail.com>
2025-11-06 14:32:50 +0530

    Update ExploreCategory.jsx

--- frontend/src/components/ExploreCategory/ExploreCategory.jsx
+++ frontend/src/components/ExploreCategory/ExploreCategory.jsx
@@ -6,7 +6,7 @@ const ExploreCategory = ({category, setCategory}) => {
 
   return (
     <div className='explore-menu'>
-      <h1>Explore Menu</h1>
+      <h1 className='explore-menu-heading'>Explore Menu</h1>
       <p className='explore-menu-text'>Enjoy top quality, great value, and easy shopping all in one place.</p>
       <div className="explore-menu-list">
         {

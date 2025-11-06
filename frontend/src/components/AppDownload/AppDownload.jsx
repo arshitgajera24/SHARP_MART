2dbfd439a905f0464f741290e06a61a0bdebeed8
Arshit Gajera <arshitgajera24@gmail.com>
2025-11-06 16:01:38 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,15 +1,14 @@
-import React, {useState} from 'react'
+import React from 'react'
 import "./AppDownload.css"
 import { assets } from '../../assets/assets'
 
 const AppDownload = () => {
-  const [loaded, setLoaded] = useState(false);
   return (
     <div className='app-download' id='app-download'>
         <p>For Better Experince Download <br /> <b>SHARP MART</b> App</p>
         <div className="app-download-platforms">
-            <img onClick={() => window.open("https://play.google.com/", "_blank")} src={assets.play_store} alt="PlayStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
-            <img onClick={() => window.open("https://www.apple.com/in/app-store/", "_blank")} src={assets.app_store} alt="AppStore" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+            <img onClick={() => window.open("https://play.google.com/", "_blank")} src={assets.play_store} alt="PlayStore" />
+            <img onClick={() => window.open("https://www.apple.com/in/app-store/", "_blank")} src={assets.app_store} alt="AppStore" />
         </div>
     </div>
   )

27cd6b60fc7db94898412b7599a3ceb001ac778f
Arshit Gajera <arshitgajera24@gmail.com>
2026-01-06 16:12:04 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,6 +23,7 @@ import ScrollToHashElement from "./components/ScrollToHashElement";
 const App = () => {
 
   const [showLogin, setShowLogin] = useState(false);
+  // Nothing
 
   useEffect(() => {
     if (showLogin) {

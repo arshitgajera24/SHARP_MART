7fc72e8766eb6849ea2e3741e80a15476910f62d
Arshit Gajera <arshitgajera24@gmail.com>
2026-01-13 22:25:21 +0530

    Update contactControllers.js

--- backend/controllers/contactControllers.js
+++ backend/controllers/contactControllers.js
@@ -3,21 +3,15 @@ import { contactValidatorSchema } from "../validators/contactValidators.js";
 
 export const sendContact = async (req, res) => {
     try {
-        
         const {data, error} = contactValidatorSchema.safeParse(req.body);
-
-        if(error)
-        {
+        if(error) {
             const errors = error.issues[0].message;
             return res.json({ success: false, error: errors });
         }
-
         const { fullName, email, subject, message } = data;
-
         const contactId = await contactServices.addNewContact({fullName, email, subject, message});
         console.log(contactId);
         res.json({success: true, message: "Message Sent Successfully"});
-
     } catch (error) {
         console.error(error);
         res.json({ success: false, error: error.message });
